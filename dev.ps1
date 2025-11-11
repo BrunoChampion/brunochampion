9759749ca08da27df5d46c4e7c0a10f725bd23c7
Bruno Champion <brunochampiongalvez@gmail.com>
2025-11-11 00:01:43 -0500

    fix: docker

--- dev.ps1
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env pwsh
-# Development environment script
-# Usage: .\dev.ps1 up -d --build
-
-docker compose -f compose.yaml -f compose.dev.yaml $args
--- prod.ps1
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env pwsh
-# Production environment script
-# Usage: .\prod.ps1 up -d --build
-
-docker compose -f compose.yaml -f compose.prod.yaml $args
